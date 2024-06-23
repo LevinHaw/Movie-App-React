407d5d120d021b04a8f1137a5947342ae0ab87dc
Leevinkie H <hawtarilevin@gmail.com>
2024-06-23 13:13:55 +0700

    add navigation

--- src/navigation/BottomTabNavigation.tsx
+++ src/navigation/BottomTabNavigation.tsx
@@ -3,43 +3,90 @@ import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
 import { Feather } from "@expo/vector-icons"
 import Favorite from "../screen/Favorite";
 import Search from "../screen/Search";
-import Home from "../screen/Home";
+import HomeScreen from "../screen/Home";
+import MovieDetail from "../screen/MovieDetail";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
-const BottomTabNavigation = (): JSX.Element => (
-    <Tab.Navigator>
-        <Tab.Screen 
-        name = "Home"
-        component={Home}
-        options = {{
-            tabBarIcon: ({color}) => (
-                <Feather name="home" size={28} color={color}/>
-            ),
-            headerShown: false,
-        }}
-        />
-        <Tab.Screen 
-        name = "Search"
-        component={Search}
-        options = {{
-            tabBarIcon: ({color}) => (
-                <Feather name="search" size={28} color={color}/>
-            ),
-            headerShown: false,
-        }}
-        />
-        <Tab.Screen 
-        name = "Favorite"
-        component={Favorite}
-        options = {{
-            tabBarIcon: ({color}) => (
-                <Feather name="heart" size={28} color={color}/>
-            ),
-            headerShown: false,
-        }}
-        />
-    </Tab.Navigator>
-)
+const HomeStackNavigator = (): JSX.Element => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="HomeScreen"
+      component={HomeScreen}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="MovieDetail"
+      component={MovieDetail}
+      options={{ headerShown: true, title: 'Movie Detail' }}
+    />
+     <Stack.Screen
+      name="Favorite"
+      component={Favorite}
+      options={{ headerShown: true, title: 'Favorite' }}
+    />
+  </Stack.Navigator>
+);
 
-export default BottomTabNavigation
+const SearchStackNavigator = (): JSX.Element => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Search"
+      component={Search}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="MovieDetail"
+      component={MovieDetail}
+      options={{ headerShown: true, title: 'Movie Detail' }}
+    />
+  </Stack.Navigator>
+);
+
+const FavoriteStackNavigator = (): JSX.Element => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="FavoriteScreen"
+      component={Favorite}
+      options={{ headerShown: false }}
+    />
+    <Stack.Screen
+      name="MovieDetail"
+      component={MovieDetail}
+      options={{ headerShown: true, title: 'Movie Detail' }}
+    />
+  </Stack.Navigator>
+);
+
+const BottomTabNavigator = (): JSX.Element => (
+  <Tab.Navigator>
+    <Tab.Screen
+      name="Home"
+      component={HomeStackNavigator}
+      options={{
+        tabBarIcon: ({ color }) => <Feather name="home" size={28} color={color} />,
+        headerShown: false,
+      }}
+    />
+    <Tab.Screen
+      name="Search"
+      component={SearchStackNavigator}
+      options={{
+        tabBarIcon: ({ color }) => <Feather name="search" size={28} color={color} />,
+        headerShown: false,
+      }}
+    />
+    <Tab.Screen
+      name="Favorite"
+      component={FavoriteStackNavigator}
+      options={{
+        tabBarIcon: ({ color }) => <Feather name="heart" size={28} color={color} />,
+        headerShown: false,
+      }}
+    />
+  </Tab.Navigator>
+);
+
+export default BottomTabNavigator;
